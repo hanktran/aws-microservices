716a8c918c89c7dfd5846783ca4d40ceac767e04
Han Tran <imhantran@gmail.com>
2022-07-05 09:36:25 +0700

    Refactoring infrastructure code

---
+++ lib/apigateway.ts
@@ -0,0 +1,39 @@
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { IFunction } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+
+interface SwnApiGatewayProps {
+  productMicroservice: IFunction;
+}
+
+export class SwnApiGateway extends Construct {
+  constructor(scope: Construct, id: string, props: SwnApiGatewayProps) {
+    super(scope, id);
+
+    // Product microservices api gateway
+    // root name = product
+
+    // GET /product
+    // POST / product
+
+    // Single product with id parameter
+    // GET /product/{id}
+    // PUT /product/{id}
+    // DELETE /product/{id}
+
+    const apigw = new LambdaRestApi(this, "productApi", {
+      restApiName: "Product Service",
+      handler: props.productMicroservice,
+      proxy: false,
+    });
+
+    const product = apigw.root.addResource("product");
+    product.addMethod("GET");
+    product.addMethod("POST");
+
+    const singleProduct = product.addResource("{id}");
+    singleProduct.addMethod("GET");
+    singleProduct.addMethod("PUT");
+    singleProduct.addMethod("DELETE");
+  }
+}
